d58a9b403653200748c0c556277d30462352e5f3
Shokochoco <shsuzu1992@gmail.com>
2021-12-10 17:05:08 +0900

    Initial Commit

---
+++ Kadai-8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai-8
+//
+//  Created by 鈴木淳子 on 2021/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
